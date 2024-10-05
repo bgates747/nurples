36648b7c1b1e5da9633b7aa9f1b25afbd2ca21bd
bgates747 <brandon.gates@gmail.com>
2024-10-05 01:14:24 -0400

    setback

--- agonutils/src/process_ttf_font.py
+++ agonutils/src/process_ttf_font.py
@@ -388,7 +388,7 @@ def char_is_defined(image, bbox):
 if __name__ == '__main__':
     # Define parameters for creating the master font
     threshold = 255-16  # Threshold for binarizing the image
-    font_name = 'scriptorium'
+    font_name = 'setback'
     font_variant = 'Regular'
 
     sources_dir = 'src/assets/ttf'
