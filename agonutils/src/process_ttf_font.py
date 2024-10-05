ef72cc3ae7a27decde5e7f213aa2ad951012d1be
bgates747 <brandon.gates@gmail.com>
2024-10-05 00:39:13 -0400

    edition_12 again

--- agonutils/src/process_ttf_font.py
+++ agonutils/src/process_ttf_font.py
@@ -388,7 +388,7 @@ def char_is_defined(image, bbox):
 if __name__ == '__main__':
     # Define parameters for creating the master font
     threshold = 255  # Threshold for binarizing the image
-    font_name = 'dopecre'
+    font_name = 'edition_12'
     font_variant = 'Regular'
 
     sources_dir = 'src/assets/ttf'
