8b469fefce92a8ee00be043e30ff7e956d02ef28
bgates747 <brandon.gates@gmail.com>
2024-10-05 00:47:40 -0400

    gamer 2 again

--- agonutils/src/process_ttf_font.py
+++ agonutils/src/process_ttf_font.py
@@ -387,8 +387,8 @@ def char_is_defined(image, bbox):
 
 if __name__ == '__main__':
     # Define parameters for creating the master font
-    threshold = 255  # Threshold for binarizing the image
-    font_name = 'fff_extras'
+    threshold = 255-16  # Threshold for binarizing the image
+    font_name = 'gamer_2'
     font_variant = 'Regular'
 
     sources_dir = 'src/assets/ttf'
