984760a7d25152d1b2d2d4cb68169fd1c16b0f89
bgates747 <brandon.gates@gmail.com>
2024-10-05 00:54:28 -0400

    delete notalot

--- agonutils/src/process_ttf_font.py
+++ agonutils/src/process_ttf_font.py
@@ -388,7 +388,7 @@ def char_is_defined(image, bbox):
 if __name__ == '__main__':
     # Define parameters for creating the master font
     threshold = 255-16  # Threshold for binarizing the image
-    font_name = 'neutrino'
+    font_name = 'notalot18'
     font_variant = 'Regular'
 
     sources_dir = 'src/assets/ttf'
