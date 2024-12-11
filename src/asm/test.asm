14d7ef6a4e497318f62162f48cd7682ed18f5b00
bgates747 <brandon.gates@gmail.com>
2024-12-10 23:15:44 -0500

    safety commit before updating maths from pac-man

--- src/asm/test.asm
+++ src/asm/test.asm
@@ -4,7 +4,8 @@
     align 64 
     db "MOS" 
     db 00h 
-    db 01h
+    db 01h 
+
 start: 
     push af
     push bc
@@ -13,131 +14,110 @@ start:
     push iy
     call init
     call main
+
 exit:
-    pop iy 
+    pop iy
     pop ix
     pop de
     pop bc
     pop af
     ld hl,0
-    ret 
+
+    ret
 
 ; API INCLUDES
     include "mos_api.inc"
     include "functions.inc"
     include "maths.inc"
     include "files.inc"
+    include "fixed168.inc"
+    include "fonts.inc"
+    include "images.inc"
+    include "timer.inc"
     include "vdu.inc"
+    include "vdu_fonts.inc"
     include "vdu_plot.inc"
+    include "vdu_sprites.inc"
 
 ; APPLICATION INCLUDES
+    include "collisions.inc"
+    include "enemies.inc"
+    include "enemy_fireball.inc"
+    include "enemy_seeker.inc"
+    include "fonts_list.inc"
     include "images_tiles_dg.inc"
+    ; include "images_tiles_xevious.inc"
     include "images_sprites.inc"
+    include "images_ui.inc"
+    include "laser.inc"
+    include "levels.inc"
+    include "levels_tileset_0.inc"
+    ; include "levels_xevious.inc"
+    include "player.inc"
+    include "sprites.inc"
+    include "state.inc"
+    include "targeting.inc"
+    
+    include "tiles.inc"
+    include "tile_pad_small.inc"
+    include "tile_turret_fireball.inc"
 
-; --- INITIALIZATION ---
+    align 256
+
+; --- MAIN PROGRAM FILE ---
+
 init:
-; clear all buffers
-    call vdu_clear_all_buffers
-
-; set up the display
-    ld a,8;+128 ; 136   320   240   64    60hz double-buffered
-    call vdu_set_screen_mode
-    xor a
-    call vdu_set_scaling
-
 
     ret
 
-; --- MAIN PROGRAM ---
 main:
-load_sprite_images:
-; initialize image loading variables
-    ld hl,0
-    ld (cur_file_idx),hl
-    ld hl,sprites_image_list
-    ld (cur_image_list),hl
-    ld bc,sprites_num_images
-; load images
-    call img_load_main
-load_tilesets:
-; initialize image loading variables
-    ld hl,0
-    ld (cur_file_idx),hl
-    ld hl,tiles_dg_image_list
-    ld (cur_image_list),hl
-    ld bc,tiles_dg_num_images
-; load images
-    call img_load_main
+    call printNewLine
+    ld bc,3*256 ; 1.000 player_xvel
+    ld de,3*256 ; 2.000 player_yvel-1
+    ; call cartesian_to_polar
+    call distance168
+    call print_s168_hl
+    call printNewLine
+    call print_s168_de
+    call printNewLine
     ret
 
-image_type: equ 0
-image_width: equ image_type+3
-image_height: equ image_width+3
-image_filesize: equ image_height+3
-image_filename: equ image_filesize+3
-image_bufferId: equ image_filename+3
-image_record_size: equ image_bufferId+3
+DEBUG_PRINT:
+    PUSH_ALL
+    ld c,0
+    ld b,0
+    call vdu_move_cursor
+    POP_ALL
+    PUSH_ALL
+    call dumpFlags
+    POP_ALL
+    PUSH_ALL
+    call dumpRegistersHex
+    ; call waitKeypress
+    POP_ALL
+    ret
 
-cur_image_list: dl 0
-cur_file_idx: dl 0
-cur_filename: dl 0
-cur_buffer_id: dl 0
+DEBUG_PRINT_TABLE:
+    PUSH_ALL
+    call vdu_home_cursor
+    ; LIST_FIELD sprite_move_program,3 ; DEBUG
+    ; LIST_FIELD sprite_type,1 ; DEBUG
 
-; inputs: bc is the number of images to load, cur_image_list set
-img_load_main:
-    xor a
-    ld (cur_file_idx),a
+    ld ix,table_base
+    call dump_sprite_record
+    call printNewLine
+    call printNewLine
 
-img_load_main_loop:
-; back up loop counter
-    push bc
+    lea ix,ix+table_bytes_per_record
+    call dump_sprite_record
+    call printNewLine
 
-; load the next image
-    call load_next_image
+    ; call waitKeypress
+    POP_ALL
+    RET
 
-; draw the most recently loaded image
-    call vdu_cls
-	ld hl,(cur_buffer_id)
-	call vdu_buff_select
-	ld bc,64
-	ld de,64
-	call vdu_plot_bmp
-
-; print current filename
-    ld hl,(cur_filename)
-    call printString
-
-; WAIT FOR KEY PRESS
+DEBUG_WAITKEYPRESS:
+    PUSH_ALL
     call waitKeypress
-
-; decrement loop counter
-    pop bc
-    dec bc
-    ld a,c
-    or a
-    jp nz,img_load_main_loop
-    ld a,b
-    or a
-    jp nz,img_load_main_loop
-    ret
-
-load_next_image:
-    ld d,image_record_size
-    ld a,(cur_file_idx)
-    ld e,a
-    mlt de
-    ld iy,(cur_image_list)
-    add iy,de
-
-    ld a,(iy+image_type) ; get image type
-    ld bc,(iy+image_width) ; get image width
-    ld de,(iy+image_height) ; get image height
-    ld ix,(iy+image_filesize) ; get image file size
-    ld hl,(iy+image_bufferId) ; get image bufferId
-    ld (cur_buffer_id),hl
-    ld iy,(iy+image_filename) ; get image filename
-    ld (cur_filename),iy
-    call vdu_load_img
-    ld iy,cur_file_idx
-    inc (iy)
-    ret
+    POP_ALL
+    RET
