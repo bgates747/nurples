9b0d4b9a974b89c96f91f3cbbc06ed29c03bc1a5
bgates747 <brandon.gates@gmail.com>
2024-12-11 20:31:53 -0500

    targeting_computer still borked. moving on for now with turrets doing naive aiming

--- src/asm/test.asm
+++ src/asm/test.asm
@@ -32,36 +32,38 @@ exit:
     include "maths.inc"
     include "files.inc"
     include "fixed168.inc"
-    include "fonts.inc"
-    include "images.inc"
+    ; include "fonts.inc"
+    ; include "images.inc"
     include "timer.inc"
     include "vdu.inc"
-    include "vdu_fonts.inc"
-    include "vdu_plot.inc"
-    include "vdu_sprites.inc"
+    ; include "vdu_fonts.inc"
+    ; include "vdu_plot.inc"
+    ; include "vdu_sprites.inc"
 
-; APPLICATION INCLUDES
-    include "collisions.inc"
-    include "enemies.inc"
-    include "enemy_fireball.inc"
-    include "enemy_seeker.inc"
-    include "fonts_list.inc"
-    include "images_tiles_dg.inc"
-    ; include "images_tiles_xevious.inc"
-    include "images_sprites.inc"
-    include "images_ui.inc"
-    include "laser.inc"
-    include "levels.inc"
-    include "levels_tileset_0.inc"
-    ; include "levels_xevious.inc"
-    include "player.inc"
-    include "sprites.inc"
-    include "state.inc"
-    include "targeting.inc"
+; ; APPLICATION INCLUDES
+;     include "collisions.inc"
+;     include "enemies.inc"
+;     include "enemy_fireball.inc"
+;     include "enemy_seeker.inc"
+;     include "fonts_list.inc"
+;     include "images_tiles_dg.inc"
+;     ; include "images_tiles_xevious.inc"
+;     include "images_sprites.inc"
+;     include "images_ui.inc"
+;     include "laser.inc"
+;     include "levels.inc"
+;     include "levels_tileset_0.inc"
+;     ; include "levels_xevious.inc"
+;     include "player.inc"
+;     include "sprites.inc"
+;     include "state.inc"
+;     include "targeting.inc"
     
-    include "tiles.inc"
-    include "tile_pad_small.inc"
-    include "tile_turret_fireball.inc"
+;     include "tiles.inc"
+;     include "tile_pad_small.inc"
+;     include "tile_turret_fireball.inc"
+
+    include "test.inc"
 
     align 256
 
@@ -72,31 +74,13 @@ init:
     ret
 
 main:
-    ld b,32 ; loop counter
-@loop:
-    push bc
-    ld h,b
-    ld l,8
-    mlt hl
-    ld h,l
-    ld l,0
-    call print_s168_hl
-    ld de,3*256
-    call polar_to_cartesian
-    push bc
-    push de
-    call print_s168_bc
-    call print_s168_de
-    pop de
-    pop bc
-
-    call cartesian_to_polar_sm
-    call print_s168_hl
-    call print_s168_de
     call printNewLine
-    pop bc
-    djnz @loop
+    call orientation_to_player
+    call targeting_computer
+
+
     ret
+
 
 DEBUG_PRINT:
     PUSH_ALL
@@ -112,28 +96,3 @@ DEBUG_PRINT:
     ; call waitKeypress
     POP_ALL
     ret
-
-DEBUG_PRINT_TABLE:
-    PUSH_ALL
-    call vdu_home_cursor
-    ; LIST_FIELD sprite_move_program,3 ; DEBUG
-    ; LIST_FIELD sprite_type,1 ; DEBUG
-
-    ld ix,table_base
-    call dump_sprite_record
-    call printNewLine
-    call printNewLine
-
-    lea ix,ix+table_bytes_per_record
-    call dump_sprite_record
-    call printNewLine
-
-    ; call waitKeypress
-    POP_ALL
-    RET
-
-DEBUG_WAITKEYPRESS:
-    PUSH_ALL
-    call waitKeypress
-    POP_ALL
-    RET
